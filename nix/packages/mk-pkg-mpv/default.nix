5579737686c25e9beae15ba9e0b5547a13b225dc
Song Chuanqi <8365457+freedom3219@user.noreply.gitee.com>
2026-06-29 19:59:05 +0800

    Fix mac libmpv init without Swift

--- nix/packages/mk-pkg-mpv/default.nix
+++ nix/packages/mk-pkg-mpv/default.nix
@@ -49,6 +49,7 @@ let
     cd $src
     patch -p1 <${../../../patches/mpv-fix-missing-objc.patch}
     patch -p1 <${../../../patches/mpv-app-bridge-without-swift.patch}
+    patch -p1 <${../../../patches/mpv-skip-mac-client-without-swift.patch}
     patch -p1 <${../../../patches/mpv-clipboard-mac-without-swift.patch}
     if [ "${os}" == "${oses.ios}" ]; then
       patch -p1 <${../../../patches/mpv-audiounit-shared-session.patch}
